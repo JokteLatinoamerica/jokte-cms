36c650a7582647a29b6de43a118d87e25b3dffe1
tuxmerlin <migueltuyare@gmail.com>
2016-03-17 18:44:37 -0300

    [fix] Codificación según versión de php (error iconv_set_enconding)

--- libraries/joomla/string/string.php
+++ libraries/joomla/string/string.php
@@ -24,11 +24,18 @@ if (extension_loaded('mbstring') || ((!strtoupper(substr(PHP_OS, 0, 3)) === 'WIN
 // Same for iconv
 if (function_exists('iconv') || ((!strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' && dl('iconv.so'))))
 {
-	// These are settings that can be set inside code
-	iconv_set_encoding("internal_encoding", "UTF-8");
-	iconv_set_encoding("input_encoding", "UTF-8");
-	iconv_set_encoding("output_encoding", "UTF-8");
+	if (PHP_VERSION_ID < 50600) {
+		// These are settings that can be set inside code
+		iconv_set_encoding("internal_encoding", "UTF-8");
+		iconv_set_encoding("input_encoding", "UTF-8");
+		iconv_set_encoding("output_encoding", "UTF-8");
+	}
+	elseif (PHP_VERSION_ID >= 50600)
+	{
+		ini_set('default_charset', 'UTF-8');
+	}
 }
+
 
 /**
  * Include the utf8 package
